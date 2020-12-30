e64ecc2875e2ba1baf2482d3d9440a3e54b18719
Tomer Levy <tomer1@post.bgu.ac.il>
2020-12-30 17:19:02 +0200

    a lot of changes

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import socket
-import sys
-from struct import pack, unpack_from
+from struct import unpack_from
 from global_params import *
 import threading
 
@@ -8,12 +7,15 @@ game_ended = False
 
 
 def get_input_and_send_to_server(tcp_socket):
+    global game_ended
     while not game_ended:
-        msg = input('')
-        tcp_socket.sendall(msg.encode('utf-8'))
+        user_input = input('')
+        if not game_ended:
+            tcp_socket.sendall(user_input.encode('utf-8'))
 
 
-if __name__ == '__main__':
+def main():
+    global game_ended
 
     # Create socket for server
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
@@ -23,6 +25,8 @@ if __name__ == '__main__':
 
     # Let'udp_socket send data through UDP protocol
     while True:
+        global game_ended
+        game_ended = False
         # send_data = input("Type some text to send =>")
         # udp_socket.sendto(send_data.encode('utf-8'), (socket.gethostname(), 2))
         # print("\n\n 1. Client Sent : ", send_data, "\n\n")
@@ -32,19 +36,22 @@ if __name__ == '__main__':
             if magic_cookie == MAGIC_COOKIE and msg_type == UDP_MSG_TYPE:
                 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 tcp_socket.connect((IP, port))
-                tcp_socket.sendall('TEAM_NAME\n'.encode('utf-8'))
+                tcp_socket.sendall('YOGA MASTERS\n'.encode('utf-8'))
                 start_game_msg = tcp_socket.recv(MAX_TCP_SIZE_BUFFER)
                 print(start_game_msg.decode('utf-8'))
+
                 input_thread = threading.Thread(target=get_input_and_send_to_server, args=(tcp_socket,))
                 input_thread.start()
                 game_ended_msg = tcp_socket.recv(MAX_TCP_SIZE_BUFFER)
                 game_ended = True
                 print(game_ended_msg.decode('utf-8'))
-
-
         except:
             pass
 
         # print("\n\n 2. Client received : ",, "\n\n")
         # close the socket
     udp_socket.close()
+
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -4,8 +4,9 @@ from concurrent.futures.thread import ThreadPoolExecutor
 import time
 import GameLogicSingleton
 from global_params import *
-from struct import pack, unpack
+from struct import pack
 import random
+
 
 
 def parse_join_tcp_msg(data: str):
@@ -49,9 +50,11 @@ def listen_and_count_team(team_name, connection):
     while True:
         try:
             data = connection.recv(MAX_TCP_SIZE_BUFFER)
-            score = len(parse_join_tcp_msg(data.decode('utf-8')))
-            GameLogicSingleton.get_instance().add_score_to_group(team_name, score_to_add=score)
+            if data is not None and len(data) > 0 and GameLogicSingleton.get_instance().game_running:
+                score = len(parse_join_tcp_msg(data.decode('utf-8')))
+                GameLogicSingleton.get_instance().add_score_to_group(team_name, score_to_add=score)
         except Exception as e:
+            print(e)
             continue
 
 
@@ -63,51 +66,51 @@ if __name__ == '__main__':
     # Bind the socket to the port
     server_address = (IP, SERVER_PORT)
     udp_socket.bind(server_address)
-    start_msg = "start msg"
+    start_msg = f"Server started, listening on IP address {IP}"
     print(start_msg)
 
     accepting_thread = threading.Thread(target=receive_tcp_offers)
     accepting_thread.start()
 
-    # main thread - sending udp broadcast offers every 1 second for 10 seconds
-    timeout = time.time() + OFFER_TIME_OUT
-    while time.time() < timeout:
-        udp_socket.sendto(pack_offer_message(), ('<broadcast>', DESTINATION_PORT))
-        print("send broadcast")
-        time.sleep(1)
-    # game is now running, cannot accept more teams
-    GameLogicSingleton.get_instance().game_running = True
-    groups = {**GameLogicSingleton.get_instance().group1, **GameLogicSingleton.get_instance().group2}
-    if len(groups) >= 1:
-        keys = list(groups.keys())
-        random.shuffle(keys)
-        thread_pool_executor = ThreadPoolExecutor(max_workers=len(keys))
-        for team_name in keys:
-            thread_pool_executor.submit(send_msg_to_players, groups[team_name],
-                                        GameLogicSingleton.get_instance().generate_welcome_msg())
-        thread_pool_executor.shutdown(wait=True)
-        thread_pool_executor = ThreadPoolExecutor(max_workers=len(keys))
-        random.shuffle(keys)
-        for team_name in keys:
-            team_connection = groups[team_name]
-            thread_pool_executor.submit(listen_and_count_team, team_name, team_connection)
+    while True:
 
-        # main thread -> wait for game to finish
+        # main thread - sending udp broadcast offers every 1 second for 10 seconds
         timeout = time.time() + OFFER_TIME_OUT
         while time.time() < timeout:
-            continue
-        thread_pool_executor.shutdown(wait=False)
+            udp_socket.sendto(pack_offer_message(), ('<broadcast>', DESTINATION_PORT))
+            print("send broadcast")
+            time.sleep(1)
+        # game is now running, cannot accept more teams
+        GameLogicSingleton.get_instance().game_running = True
+        groups = {**GameLogicSingleton.get_instance().group1, **GameLogicSingleton.get_instance().group2}
+        if len(groups) >= 1:
+            keys = list(groups.keys())
+            random.shuffle(keys)
+            thread_pool_executor = ThreadPoolExecutor(max_workers=len(keys))
+            for team_name in keys:
+                thread_pool_executor.submit(send_msg_to_players, groups[team_name],
+                                            GameLogicSingleton.get_instance().generate_welcome_msg())
+            thread_pool_executor.shutdown(wait=True)
+            thread_pool_executor = ThreadPoolExecutor(max_workers=len(keys))
+            random.shuffle(keys)
+            for team_name in keys:
+                team_connection = groups[team_name]
+                thread_pool_executor.submit(listen_and_count_team, team_name, team_connection)
 
-        random.shuffle(keys)
-        thread_pool_executor = ThreadPoolExecutor(max_workers=len(keys))
-        for team_name in keys:
-            thread_pool_executor.submit(send_msg_to_players, groups[team_name],
-                                        GameLogicSingleton.get_instance().generate_end_msg())
+            # main thread -> wait for game to finish
+            timeout = time.time() + OFFER_TIME_OUT
+            while time.time() < timeout:
+                continue
+            thread_pool_executor.shutdown(wait=False)
 
-    # while True:
-    #     print("####### Server is listening #######")
-    #     data, address = udp_socket.recvfrom(4096)
-    #     print("\n\n 2. Server received: ", data.decode('utf-8'), "\n\n")
-    #     send_data = input("Type some text to send => ")
-    #     udp_socket.sendto(send_data.encode('utf-8'), address)
-    #     print("\n\n 1. Server sent : ", send_data, "\n\n")
+            random.shuffle(keys)
+            thread_pool_executor = ThreadPoolExecutor(max_workers=len(keys))
+            for team_name in keys:
+                thread_pool_executor.submit(send_msg_to_players, groups[team_name],
+                                            GameLogicSingleton.get_instance().generate_end_msg())
+
+            thread_pool_executor.shutdown(wait=True)
+            GameLogicSingleton.get_instance().reset()
+
+
+
